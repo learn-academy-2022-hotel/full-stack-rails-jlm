03c0bba7e1bae948b95073e6c2f97158314afbb3
MLMunoz85 <matthew.l.munoz@gmail.com>
2023-01-19 11:22:47 -0800

    completed user story 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'blogs' => 'blog#index', as: 'blogs'
+  post 'blogs' => 'blog#create'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
   get 'blogs/:id' => 'blog#show', as: 'blog'
+
 end
